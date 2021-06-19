3f07e2fcba8533660ea3d8b54ca4c2242241f202
Nicolai Negru <negru.n@gmail.com>
2021-06-19 14:26:34 +0100

    move ctrl on top of left shift

--- keymap.c
+++ keymap.c
@@ -18,20 +18,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |BackSP|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  DEL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |CTL/DEL|  A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |Shft( |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Shft) |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |CTL ~ | Alt |  Cmd | /Space  /       \Enter \  | LOWER |   \  |   =  |
+ *                   |  Alt |  ~  |  Cmd | /Space  /       \Enter \  | LOWER |   \  |   =  |
  *                   `----------------------------'          '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   		KC_6,	KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   		KC_Y,	KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_DEL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                   		KC_H,	KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, 	KC_LBRC,  		KC_RBRC, 	KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, \
-                    CTL_T(KC_GRV),    KC_LALT, KC_LGUI,	KC_SPC,  	KC_ENT, MO(_LOWER), KC_BSLS, KC_EQL \
+  KC_GESC,  		KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   			KC_6,		KC_7,    	KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,   		KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   			KC_Y,		KC_U,    	KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  CTL_T(KC_DEL),	KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                			KC_H,		KC_J,    	KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSPO,  		KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, 	KC_LBRC,	KC_RBRC, 	KC_N,   	KC_M,    	KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, \
+									 KC_LALT,  KC_GRV, KC_LGUI, KC_SPC,  	KC_ENT, 	MO(_LOWER), KC_BSLS, 	KC_EQL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------|      | LEFT | DOWN | RIGHT|      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * |Shft {|      |      |      |      |      |-------|    |-------|      |      |      |      |      |Shft }|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /      /        \      \  |      |      |       |
  *                   `----------------------------'           '------''--------------------'
