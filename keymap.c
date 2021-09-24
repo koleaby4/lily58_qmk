60aa07509fca1cebcbb9752b4a147f30f4d910cd
Nicolai Negru <negru.n@gmail.com>
2021-09-24 18:17:40 +0100

    moved GUI the the left and brought CTRL to bottom

--- keymap.c
+++ keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |Shft( |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Shft) |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |  Alt |  ~  |  Cmd | /Space  /       \Enter \  | LOWER |   \  |   =  |
+ *                   |  Alt | GUI |  Ctrl | /Space  /       \Enter \  | LOWER |  \|  |   `  |
  *                   `----------------------------'          '------''--------------------'
  */
 
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   		KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   			KC_Y,		KC_U,    	KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CTL_T(KC_DEL),	KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                			KC_H,		KC_J,    	KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSPO,  		KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, 	KC_LBRC,	KC_RBRC, 	KC_N,   	KC_M,    	KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, \
-									 KC_LALT,  KC_GRV, KC_LGUI, KC_SPC,  	KC_ENT, 	MO(_LOWER), KC_BSLS, 	KC_EQL \
+									 KC_LALT,  KC_LGUI, KC_LCTL, KC_SPC,  	KC_ENT, 	MO(_LOWER), KC_NUBS, 	KC_GRV \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
