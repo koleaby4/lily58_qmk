de40dce1c52d8a4b7664153e8622d0ce28762611
Nicolai Negru <negru.n@gmail.com>
2024-08-18 12:30:38 +0100

    spaces instead of tabs

--- keymap.c
+++ keymap.c
@@ -27,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,       KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                     		KC_F6,  KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-  KC_TAB,       KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    		KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_DEL,		KC_A, 	KC_S,    KC_D,    KC_F,    KC_G,                 	    	KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  SC_LSPO,      KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_NUHS,    KC_INS, 	KC_N,	KC_M,     KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, \
-								 KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,     KC_ENT, 	LT(_L1, KC_LBRC),LT(_L2, KC_RBRC),  KC_NUHS  \
+  KC_ESC,       KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,  KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+  KC_TAB,       KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_DEL,       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  SC_LSPO,      KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_NUHS,    KC_INS,   KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, \
+                                 KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,     KC_ENT,   LT(_L1, KC_LBRC),LT(_L2, KC_RBRC),  KC_NUHS  \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_L1] = LAYOUT( \
-  _______,    _______, _______, 	_______, 	_______, 	_______,            	   		_______, _______, _______, _______, _______, KC_F12,  \
-  CW_TOGG,    KC_1,    KC_2,    	KC_3,    	KC_4,    	KC_5,               	   		KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______,    KC_LBRC, S(KC_LBRC), 	S(KC_RBRC), KC_RBRC,  	KC_EQL,            	  			KC_PGUP, KC_HOME, KC_UP  , KC_END,  KC_MINS, KC_GRV,  \
-  _______,    _______, _______, 	_______, 	_______, 	_______,  	_______,  _______,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NUBS, _______, \
-												_______, 	_______,	_______,  _______, 	_______,  _______, _______, _______ \
+  _______,    _______, _______,     _______,    _______,    _______,                        _______, _______, _______, _______, _______, KC_F12,  \
+  CW_TOGG,    KC_1,    KC_2,        KC_3,       KC_4,       KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  _______,    KC_LBRC, S(KC_LBRC),  S(KC_RBRC), KC_RBRC,    KC_EQL,                         KC_PGUP, KC_HOME, KC_UP  , KC_END,  KC_MINS, KC_GRV,  \
+  _______,    _______, _______,     _______,    _______,    _______,    _______,  _______,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NUBS, _______, \
+                                                _______,    _______,    _______,  _______,  _______,  _______, _______, _______ \
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
