6765a00c07e063eacc720500f5ebd9601aae1b7c
Nicolai Negru <negru.n@gmail.com>
2024-08-18 12:26:01 +0100

    combine layers with brackets

--- keymap.c
+++ keymap.c
@@ -21,9 +21,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  DEL |  A   |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  # ~  |    |  INS  |------+------+------+------+------+------|
  * |Shft( |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Shft) |
- * '-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | Alt  | GUI | Ctrl | /Space  /        \Enter \  | _L1  | _L2  |  # ~  |
- *                   '----------------------------'          '----------------------------'
+ * '-----------------------------------------/      /     \       \-----------------------------------------'
+ *                   |  Alt | GUI | Ctrl |  / Space /      \ Enter \  | L1 [  | L2 ] |  # ~  |
+ *                   '----------------------------'         '----------------------------'
  */
 
  [_QWERTY] = LAYOUT( \
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,       KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    		KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_DEL,		KC_A, 	KC_S,    KC_D,    KC_F,    KC_G,                 	    	KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   SC_LSPO,      KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_NUHS,    KC_INS, 	KC_N,	KC_M,     KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, \
-								 KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,     KC_ENT, 	MO(_L1),MO(_L2),  KC_NUHS  \
+								 KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,     KC_ENT, 	LT(_L1, KC_LBRC),LT(_L2, KC_RBRC),  KC_NUHS  \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
